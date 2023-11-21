e7b202a89239c5a58e0357752b1e7d80e11d4a56
Alejandro <Alejandro.meno@hotmail.com>
2023-11-21 09:09:11 +0100

    feat: http response control

--- gestiontienda/src/main/java/com/cursojava/gestiontienda/servlet/EliminarServlet.java
+++ gestiontienda/src/main/java/com/cursojava/gestiontienda/servlet/EliminarServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class EliminarServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String nombreProducto = request.getParameter("nombre");
 
         // Lógica para eliminar el producto de la lista en la sesión
